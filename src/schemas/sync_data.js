70cb5186bcaafdc8317bcda042f59103e7989511
poyang31 <poyang31@yahoo.com.tw>
2023-01-07 18:49:45 +0800

    update schema

--- src/schemas/sync_data.js
+++ src/schemas/sync_data.js
@@ -7,8 +7,6 @@ const Schema = mongoose.Schema;
 module.exports = new Schema({
     _id: String,
     content: String,
-    created_at: Number,
-    updated_at: Number,
 }, {
     timestamps: true,
 });
